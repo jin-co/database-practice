9ea70be0485d4546bdd0cc98ba287953954ce074
jin-co <lilijk@hanmail.net>
2021-10-04 13:39:57 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -616,3 +616,31 @@ FROM invoices
 WHERE invoice_total > 
 	(SELECT AVG(invoice_total)
     FROM invoices);
+
+SELECT invoice_number, invoice_date, invoice_total
+FROM invoices JOIN vendors
+	USING(vendor_id)
+WHERE vendor_state = 'CA'
+ORDER BY invoice_date;
+
+SELECT invoice_number, invoice_date, invoice_total
+FROM invoices 
+WHERE vendor_id IN (SELECT vendor_id FROM
+		vendors WHERE vendor_state = 'CA')
+ORDER BY invoice_date;
+
+SELECT vendor_id, vendor_name, vendor_state
+FROM vendors
+WHERE vendor_id NOT IN
+	(SELECT DISTINCT vendor_id
+    FROM invoices)
+ORDER BY vendor_id;
+
+SELECT v.vendor_id, vendor_name, vendor_state
+FROM vendors v LEFT JOIN invoices i 
+	USING(vendor_id)
+WHERE i.vendor_id IS NULL
+ORDER BY vendor_id;
+
+-- comparison operator
+-- - if used with ANY, SOME, ALL keyword -> returns a list
