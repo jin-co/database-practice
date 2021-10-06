b69211b9f9c07687533f5f49a0f4391798f4df01
jin-co <lilijk@hanmail.net>
2021-10-06 12:16:32 +0900

    chore: udpate

--- daily_practice.sql
+++ daily_practice.sql
@@ -692,3 +692,9 @@ WHERE NOT EXISTS
 	(SELECT *
     FROM invoices
     WHERE vendor_id = vendors.vendor_id);
+    
+SELECT vendor_name,
+	(SELECT MAX(invoice_date) FROM invoices
+	WHERE vendor_id = vendors.vendor_id) AS latest_inv
+FROM vendors
+ORDER BY latest_inv DESC;
