4c2ff949510b217be10434c9cffce5ad86e9b8d4
jin-co <lilijk@hanmail.net>
2021-10-08 13:59:15 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -841,10 +841,6 @@ FROM general_ledger_accounts g
 WHERE i.invoice_id IS NULL;
 
 -- 4
-SELECT * FROM vendors;
-SELECT * FROM invoices;
-SELECT * FROM invoice_line_items;
-
 SELECT vendor_name, invoice_id, invoice_sequence, line_item_amount FROM vendors
 	JOIN invoices USING(vendor_id)
     JOIN invoice_line_items USING(invoice_id)
@@ -853,26 +849,32 @@ WHERE invoice_id IN (
 	WHERE invoice_sequence > 1)
 ORDER BY vendor_name, invoice_id, invoice_sequence;
 
-SELECT vendor_name, invoice_id, invoice_sequence, line_item_amount FROM vendors
-	JOIN invoices USING(vendor_id)
-    JOIN invoice_line_items li USING(invoice_id)
-WHERE li.invoice_sequence > 1
-ORDER BY vendor_name, invoice_id, invoice_sequence;
+-- 5
+SELECT SUM(pay_total) AS total FROM (
+	SELECT invoice_id, 
+		   MAX(invoice_total) AS pay_total
+	FROM invoices
+	WHERE invoice_total - payment_total - credit_total > 0
+	GROUP BY vendor_id) t;
 
-SELECT vendor_name, i.invoice_id, invoice_sequence, line_item_amount
-FROM vendors v JOIN invoices i
-  ON v.vendor_id = i.vendor_id
-JOIN invoice_line_items li
-  ON i.invoice_id = li.invoice_id
-WHERE i.invoice_id IN
-      (SELECT DISTINCT invoice_id
-       FROM invoice_line_items               
-       WHERE invoice_sequence > 1)
-ORDER BY vendor_name, i.invoice_id, invoice_sequence;
+-- 6
+SELECT * FROM vendors;
+SELECT vendor_name, vendor_city, vendor_state FROM vendors;
 
-SELECT DISTINCT *
-       FROM invoice_line_items               
-       WHERE invoice_sequence > 1;
+SELECT vendor_name, vendor_city, vendor_state
+FROM vendors
+WHERE CONCAT(vendor_state, vendor_city) NOT IN 
+    (SELECT CONCAT(vendor_state, vendor_city) as vendor_city_state
+     FROM vendors
+     GROUP BY vendor_city_state
+     HAVING COUNT(*) > 1)
+ORDER BY vendor_state, vendor_city;
+
+SELECT CONCAT(vendor_state, vendor_city) as vendor_city_state
+     FROM vendors
+     GROUP BY vendor_city_state
+     HAVING COUNT(*) > 1;
+
 
 
 
