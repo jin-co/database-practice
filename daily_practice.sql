ad041f3da5572e97ac77c84d4c1d8c6390764186
jin-co <lilijk@hanmail.net>
2021-10-08 13:40:05 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -848,12 +848,31 @@ SELECT * FROM invoice_line_items;
 SELECT vendor_name, invoice_id, invoice_sequence, line_item_amount FROM vendors
 	JOIN invoices USING(vendor_id)
     JOIN invoice_line_items USING(invoice_id)
-WHERE invoice_sequence > 1
+WHERE invoice_id IN (
+	SELECT invoice_id FROM invoice_line_items
+	WHERE invoice_sequence > 1)
 ORDER BY vendor_name, invoice_id, invoice_sequence;
 
+SELECT vendor_name, invoice_id, invoice_sequence, line_item_amount FROM vendors
+	JOIN invoices USING(vendor_id)
+    JOIN invoice_line_items li USING(invoice_id)
+WHERE li.invoice_sequence > 1
+ORDER BY vendor_name, invoice_id, invoice_sequence;
 
-SELECT invoice_sequence FROM invoice_line_items
-WHERE invoice_sequence > 1;
+SELECT vendor_name, i.invoice_id, invoice_sequence, line_item_amount
+FROM vendors v JOIN invoices i
+  ON v.vendor_id = i.vendor_id
+JOIN invoice_line_items li
+  ON i.invoice_id = li.invoice_id
+WHERE i.invoice_id IN
+      (SELECT DISTINCT invoice_id
+       FROM invoice_line_items               
+       WHERE invoice_sequence > 1)
+ORDER BY vendor_name, i.invoice_id, invoice_sequence;
+
+SELECT DISTINCT *
+       FROM invoice_line_items               
+       WHERE invoice_sequence > 1;
 
 
 
