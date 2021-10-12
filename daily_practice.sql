a78860a61a10989f371708aaab088c7bdc67b898
jin-co <lilijk@hanmail.net>
2021-10-12 22:33:25 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -920,7 +920,27 @@ WITH max_invoice AS
 SELECT SUM(invoice_max) AS sum_of_maximums
 FROM max_invoice;
 
+/* data type */
+-- int
+-- float(m, d) : m -> how long(in total) / d -> how long to the decimal point // appoximate -> rounds
+-- decimal(m, d) : precise -> dosen't round
+-- char(n) : fixed length, 'n' is the maximum number of charater between 0 ~ 255 // MySQL must reserve 4 byte for each character so 'n * 4'
+-- varchar(n) : Variable length, 'n' 'n' is the maximum number of charater between 0 ~ 255 // for English and Latin, needs 1 byte more so 'n' + 1
+-- enum('m', 'f') : for defined value
+-- boolean
+-- date : date(YYYY-MM-DD)
+-- datetime : date and the time(YYYY-MM-DD HH-MM-SS)
+-- time : time(HHH-MM-SS)
+-- year : year(YYYY)
 
+/* data usages under utf8mb4 character set */
+-- 1 byte: Latin, digits, punctuations
+-- 2 byte: European,  Middle East letters
+-- 3 byte: Korean, Chinese, Japanese
+-- 4 byte: Emojis, icons
+
+/* tips */
+-- there is a concept of return type(value, list) so use it accordingly
 
 
 
