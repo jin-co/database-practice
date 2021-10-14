68a19c2ca96e4880bfdfab02d3f84e750798cb72
jin-co <lilijk@hanmail.net>
2021-10-15 01:01:51 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -971,6 +971,58 @@ WHERE category_id = 5;
 DELETE FROM categories
 WHERE category_id = 5;
 
+-- 2
+SELECT * FROM products;
+SELECT * FROM order_items;
+SELECT * FROM categories;
+
+INSERT INTO products(
+	   product_id,
+	   category_id,
+       product_code,
+       product_name,
+       description,
+       list_price,
+       discount_percent,
+       date_added)
+       VALUES(
+       DEFAULT,
+       4,
+       "dgx_640",
+       "Yamaha DGX 640 88-Key Digital Piano",
+       "Long description to come.",
+       799.99,
+       0,
+       sysdate());
+
+UPDATE products SET discount_percent = 35 WHERE product_name = "Yamaha DGX 640 88-Key Digital Piano";
+
+DELETE FROM products WHERE product_id = 13;
+/*
+INSERT to the Products table:
+product_id: The next automatically generated ID 
+category_id: 4
+product_code: dgx_640
+product_name: Yamaha DGX 640 88-Key Digital Piano
+description: Long description to come.
+list_price: 799.99
+discount_percent: 0
+date_added: Today’s date/time.
+Use a column list for this statement.
+
+UPDATE discount_percent column from 0% to 35%.
+
+SELECT statement that displays your updated record. Save this output 
+
+DELETE the row that you added to the Categories 
+When you execute this statement, it will produce an error since 
+the category has related rows in the Products table. To fix that, precede the DELETE 
+statement with another DELETE statement that deletes all products in this category. 
+(Remember that to code two or more statements in a script, you must end each 
+statement with a semicolon.)
+Save all sql statements above in one file, query2.sql
+*/
+
 
 
 
