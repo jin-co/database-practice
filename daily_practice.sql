f741dc83e2efa3cbd0cb37bbd3cf7241046b0dfd
jin-co <lilijk@hanmail.net>
2021-10-19 13:37:38 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -993,8 +993,16 @@ ORDER BY list_price DESC;
 SELECT CONCAT(first_name, ', ', last_name) AS full_name,
 	   substr(last_name, 1, 1)
 FROM customers 
-WHERE substr(last_name, 1, 1) BETWEEN 'm' AND 'z'
+WHERE last_name BETWEEN 'm%' AND 'z%'
 ORDER BY last_name;
+
+/*
+My SQL
+implicit comversion
+string  in a numeric expression 
+-> if the string starts with a letter of character: 0
+-> if 
+*/
 
 
 
