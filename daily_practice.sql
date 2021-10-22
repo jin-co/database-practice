cb36d8eed9efedbaf78ac4bdde04f8814cb13658
jin-co <lilijk@hanmail.net>
2021-10-22 11:26:51 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -1203,9 +1203,18 @@ WHERE vendor_id IN (SELECT vendor_id FROM vendors
 WHERE vendor_state = 'CA')
 ORDER BY invoice_date;
 
-
-
 SELECT * FROM vendors;
 SELECT * FROM invoices;
+
+SELECT invoice_number, invoice_date,
+	   invoice_total - payment_total - credit_total AS balance_due
+FROM invoices
+WHERE invoice_total - payment_total - credit_total > 0
+  AND invoice_total - payment_total - credit_total <
+	(
+		SELECT AVG(invoice_total - payment_total - credit_total)
+        FROM invoices
+        WHERE invoice_total - payment_total - credit_total > 0
+	);
 
 
