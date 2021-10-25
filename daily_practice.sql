3a78faa1b5bc37d7ad83c7eafeb77618d7d13931
jin-co <lilijk@hanmail.net>
2021-10-25 12:09:30 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -1259,3 +1259,7 @@ WHERE invoice_total >
      FROM invoices
      WHERE vendor_id = i.vendor_id)
 ORDER BY vendor_id, invoice_total;
+
+SELECT vendor_id, vendor_name, vendor_state
+FROM vendors
+WHERE EXISTS (SELECT * FROM invoices WHERE vendor_id = vendors.vendor_id);
