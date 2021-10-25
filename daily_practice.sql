b4684bb0afcb49b70222de54af8ad8c3701d7a9b
jin-co <lilijk@hanmail.net>
2021-10-25 12:15:48 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -1263,3 +1263,9 @@ ORDER BY vendor_id, invoice_total;
 SELECT vendor_id, vendor_name, vendor_state
 FROM vendors
 WHERE EXISTS (SELECT * FROM invoices WHERE vendor_id = vendors.vendor_id);
+
+SELECT vendor_name, MAX(invoice_date) AS latest_inv
+FROM vendors v
+	LEFT JOIN invoices i ON v.vendor_id = i.vendor_id
+GROUP BY vendor_name
+ORDER BY latest_inv DESC;
