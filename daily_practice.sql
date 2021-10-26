d69a4b26cab3ee22e4ce3b53e11bd7c79a46bb2a
jin-co <lilijk@hanmail.net>
2021-10-26 11:40:14 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -1275,3 +1275,10 @@ SELECT vendor_name,
     WHERE vendor_id = vendors.vendor_id)
     FROM vendors;
 
+SELECT vendor_name, MAX(invoice_date) FROM vendors
+left JOIN invoices USING(vendor_id)
+GROUP BY vendor_name;
+
+SELECT vendor_name,
+	(SELECT MAX(invoice_date) FROM invoices WHERE vendor_id = vendors.vendor_id)
+FROM vendors;
