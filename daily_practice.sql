f16472772626d011674386f680bb646461d4538a
jin-co <lilijk@hanmail.net>
2021-10-28 11:37:24 +0900

    chore: update

--- daily_practice.sql
+++ daily_practice.sql
@@ -1348,3 +1348,20 @@ WITH summary AS
         FROM summary JOIN top_in_state
         ON summary.vendor_state = top_in_state.vendor_state AND
         summary.sum_of_invoices = top_in_state.sum_of_invoices;
+        
+WITH RECURSIVE employees_cte AS
+(
+	SELECT employee_id,
+	CONCAT(first_name, ' ', last_name) AS employee_name,
+    1 AS ranking
+    FROM employees
+    WHERE manager_id IS NULL
+    UNION ALL
+    SELECT employees.employee_id,
+    CONCAT(first_name, ' ', last_name),
+    ranking + 1
+    FROM employees
+    JOIN employees_cte
+    ON employees.manager_id = employees_cte.employee_id)
+    SELECT * FROM employees_cte;
+    
