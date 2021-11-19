3487292d3b5f679b5c2337b54099e3874eaea537
jin-co <lilijk@hanmail.net>
2021-11-19 12:09:36 +0900

    chore: update

--- MySQL_Cheatsheet/questions/murachAndConestoga.sql
+++ MySQL_Cheatsheet/questions/murachAndConestoga.sql
@@ -964,11 +964,41 @@ USE ex;
 SELECT start_date,
        DATE_FORMAT(start_date, '%b/%d/%y'),
        DATE_FORMAT(start_date, '%c/%e/%y'),
-       DATE_FORMAT(start_date, '%h:%i %p')
-       
+       DATE_FORMAT(start_date, '%h:%i %p')       
+FROM date_sample;
+
+SELECT start_date, 
+    DATE_FORMAT(start_date, '%b/%d/%y') AS format1, 
+    DATE_FORMAT(start_date, '%c/%e/%y') AS format2, 
+    DATE_FORMAT(start_date, '%l:%i %p') AS twelve_hour,
+    DATE_FORMAT(start_date, '%c/%e/%y %l:%i %p') AS format3 
 FROM date_sample;
 
 -- c9-3
+USE ap;
+SELECT vendor_name,
+	   UPPER(vendor_name),
+	   vendor_phone,
+	   COALESCE(RIGHT(vendor_phone, 4), 'nv'),
+	   COALESCE(RIGHT(vendor_phone, 4), 'nv')
+       
+FROM vendors;
+
+SELECT vendor_name,
+    UPPER(vendor_name) AS vendor_name_upper,
+    vendor_phone,
+    SUBSTRING(vendor_phone, 11) AS last_digits,
+    REPLACE(REPLACE(REPLACE(vendor_phone, '(', ''), ') ', '.'), '-', '.') AS phone_with_dots,
+    IF(LOCATE(' ', vendor_name) = 0,
+        '',
+		IF(LOCATE(' ', vendor_name, LOCATE(' ', vendor_name) + 1) = 0,
+			SUBSTRING(vendor_name, LOCATE(' ', vendor_name) + 1),
+			SUBSTRING(vendor_name,
+				LOCATE(' ', vendor_name) + 1,
+                LOCATE(' ', vendor_name, LOCATE(' ', vendor_name) + 1) - LOCATE(' ', vendor_name))))
+    AS second_word
+FROM vendors;
+
 -- c9-4
 -- c9-5
 -- c9-6
