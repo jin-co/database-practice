4f85cb0d9a73bd9a69db646ba79a1ea5155e5722
jin-co <lilijk@hanmail.net>
2021-11-21 13:45:01 +0900

    chore: update

--- MySQL_Cheatsheet/questions/murachAndConestoga.sql
+++ MySQL_Cheatsheet/questions/murachAndConestoga.sql
@@ -1001,6 +1001,16 @@ SELECT vendor_name,
 FROM vendors;
 
 -- c9-4
+
+SELECT invoice_number,
+       invoice_date,
+       DATE_ADD(invoice_date, INTERVAL 30 DAY) AS date_plus_30_days,
+       payment_date,
+       DATEDIFF(payment_date, invoice_date) AS days_to_pay,
+       MONTH(invoice_date) AS "month",
+       YEAR(invoice_date) AS "year"
+FROM invoices
+WHERE invoice_date > '2018-04-30' AND invoice_date < '2018-06-01'
 -- c9-5
 -- c9-6
 SELECT * FROM invoices;
