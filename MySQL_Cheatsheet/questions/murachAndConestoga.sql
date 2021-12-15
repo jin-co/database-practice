bfd5d9a82901cc506ef4703fcfe527c42d14a7b1
jin-co <lilijk@hanmail.net>
2021-12-15 11:41:43 +0900

    chore: update

--- MySQL_Cheatsheet/questions/murachAndConestoga.sql
+++ MySQL_Cheatsheet/questions/murachAndConestoga.sql
@@ -414,8 +414,30 @@ GROUP BY account_description
 HAVING line_item_count > 1
 ORDER BY line_item_amount_sum DESC;
 
--- ch6-1
--- ch6-1
+-- ch6-6
+SELECT account_number,
+       SUM(line_item_amount)
+FROM general_ledger_accounts JOIN invoice_line_items USING(account_number)
+GROUP BY account_number
+WITH ROLLUP;
+
+SELECT account_number, SUM(line_item_amount) AS line_item_sum
+FROM invoice_line_items
+GROUP BY account_number WITH ROLLUP;
+
+-- ch6-7
+SELECT vendor_name,
+       COUNT(DISTINCT li.account_number) AS number_of_gl_accounts
+FROM vendors v 
+  JOIN invoices i
+    ON v.vendor_id = i.vendor_id
+  JOIN invoice_line_items li
+    ON i.invoice_id = li.invoice_id
+GROUP BY vendor_name
+HAVING number_of_gl_accounts > 1
+ORDER BY vendor_name;
+
+
 -- ch6-1
 -- ch6-1
 -- ch6-1
