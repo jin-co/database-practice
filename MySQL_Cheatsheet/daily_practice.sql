f40cfc36e142af3f2143e8dd67f26e0fed92f939
jin-co <lilijk@hanmail.net>
2022-01-13 12:09:49 +0900

    chore: update

--- MySQL_Cheatsheet/daily_practice.sql
+++ MySQL_Cheatsheet/daily_practice.sql
@@ -1495,7 +1495,7 @@ UPDATE jobs, employees
 	employees.commission_pct = employees.commission_pct + .10
 	WHERE jobs.job_id = 'PU_CLERK' AND employees.job_id = 'PU_CLERK';
 
--- subquery 16
+-- subquery 19
 SET @i = 0; 
 SELECT i, employee_id  -- a query to fetch even numbered records from employees table.
 FROM (SELECT @i := @i + 1 AS i, employee_id FROM employees)
@@ -1503,6 +1503,6 @@ a WHERE MOD(a.i, 2) = 0;
 
 SELECT DISTINCT salary 
 FROM employees e1 
-WHERE 5 = (SELECT COUNT(DISTINCT salary) 
+WHERE 5 = (SELECT COUNT(DISTINCT salary) -- 5th maximum salary in the employees table
 FROM employees  e2 
 WHERE e2.salary >= e1.salary);
