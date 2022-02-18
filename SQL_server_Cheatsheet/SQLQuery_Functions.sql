36336d017f4243290aa4edddfd1700c1a964f308
jin-co <lilijk@hanmail.net>
2022-02-18 19:59:06 +0900

    Update

--- SQL_server_Cheatsheet/SQLQuery_Functions.sql
+++ SQL_server_Cheatsheet/SQLQuery_Functions.sql
@@ -1,4 +1,24 @@
 -- every schema has function list: Programmability -> Functions
+/*============= Configuration =============*/
+SELECT @@SERVERNAME;
+
+SELECT @@VERSION;
+
+SELECT @@CONNECTIONS, @@CPU_BUSY, @@CONNECTIONS;
+
+/*============= STRING =============*/
+-- UPPER
+SELECT UPPER(name) FROM Movies;
+
+-- LOWER
+SELECT LOWER(name) FROM Movies;
+
+-- LEN
+-- -> returns the number of characters in a string
+SELECT LEN(name) FROM Movies;
+
+-- SOUNDEX
+SELECT name FROM Movies WHERE SOUNDEX(name) = SOUNDEX('TOM');
 
 /*============= AGREGATION =============*/
 
@@ -15,3 +35,25 @@ SELECT SUM(price) FROM movies;
 /*------------- GROUP BY -------------*/
 -- Columns that are not aggregation need to be in the 'GROUP BY'
 SELECT SUM(price), name FROM movies GROUP BY name; 
+
+/*============= DATE =============*/
+-- GETDATE
+SELECT GETDATE();
+
+-- DAY
+SELECT DAY(GETDATE());
+
+-- MONTH
+SELECT MONTH(GETDATE());
+
+-- YEAR
+SELECT YEAR(GETDATE());
+
+-- DATEPART
+SELECT DATEPART(WEEKDAY, GETDATE());
+
+-- DATENAME
+SELECT DATENAME(WEEKDAY, GETDATE());
+
+-- DATEADD(what, how much, target)
+SELECT DATEADD(DAY, 30, GETDATE());
