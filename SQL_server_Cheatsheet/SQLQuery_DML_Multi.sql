28f975a24bc5a0769b64863418fc800769e840b1
jin-co <lilijk@hanmail.net>
2022-02-18 18:35:13 +0900

    Update

--- SQL_server_Cheatsheet/SQLQuery_DML_Multi.sql
+++ SQL_server_Cheatsheet/SQLQuery_DML_Multi.sql
@@ -1,5 +1,14 @@
 SELECT * FROM Bills b
 JOIN Suppliers s ON b.SupplierId = s.SupplierId; -- inner join
 
-/* (INNER) JOIN */
+/* JOIN */
+-- (INNER) JOIN
 SELECT m.name FROM movies m JOIN cinemas c ON (m.CinemaId = c.CinemaId);
+
+-- LEFT OUTER JOIN
+SELECT m.name FROM movies m LEFT OUTER JOIN cinemas c ON (m.CinemaId = c.CinemaId);
+
+-- RIGHT OUTER JOIN
+SELECT m.name FROM movies m RIGHT OUTER JOIN cinemas c ON (m.CinemaId = c.CinemaId);
+
+/* SUBQUERY */
