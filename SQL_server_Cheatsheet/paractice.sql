08bba6066d9f2d9384835a15c5f95c9e46ff1a47
jin-co <lilijk@hanmail.net>
2022-04-18 20:30:54 -0400

    Update

--- SQL_server_Cheatsheet/paractice.sql
+++ SQL_server_Cheatsheet/paractice.sql
@@ -17,3 +17,25 @@ INNER JOIN Invoices_Payments ip ON i.invoice_payment_id = ip.id;
 
 SELECT * FROM Invoices i
 LEFT JOIN Invoices_Payments ip ON i.invoice_payment_id = ip.id;
+
+
+SELECT * FROM bills;
+SELECT * FROM bill_payments;
+
+-- select all records in the bills table that have payments
+SELECT * FROM bills WHERE bill_payment_id IS NOT NULL;
+SELECT * FROM bills b 
+RIGHT JOIN bill_payments bp 
+ON  b.bill_payment_id = bp.id;
+
+-- select all records in the bills table that DO NOT have payments
+SELECT * FROM bills WHERE bill_payment_id IS NULL;
+
+-- show the totals of all transactions 
+-- in the bills_details table for each bill_id
+SELECT * FROM bills_details;
+SELECT * FROM bills;
+
+SELECT SUM(transaction_amount_bill) FROM bills_details bd
+JOIN bills b ON b.id = bd.bill_id
+GROUP BY b.id;
