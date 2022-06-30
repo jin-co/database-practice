cd12a2031bbf0612ccaaaf8cf70f035c0842133b
jin-co <lilijk@hanmail.net>
2022-06-30 20:56:59 +0900

    Added term table

--- MySQL_Cheatsheet/ERD/itcpa_db_script.sql
+++ MySQL_Cheatsheet/ERD/itcpa_db_script.sql
@@ -1,10 +1,24 @@
-DROP TABLE IF EXISTS student;
 
+-- student
+DROP TABLE IF EXISTS student;
 CREATE TABLE student (
 	student_id INT(30) PRIMARY KEY AUTO_INCREMENT,
     student_first_name VARCHAR(50) NOT NULL,
     student_last_name VARCHAR(50) NOT NULL,
+    student_email VARCHAR(50) NOT NULL,
     student_number VARCHAR(50) NOT NULL
-)
+);
 
+INSERT INTO student VALUES (
+	DEFAULT, 'tom', 'jack', '#202020'
+);
+
+-- term
+DROP TABLE IF EXISTS school_term;
+CREATE TABLE school_term (
+	school_term_id INT(30) PRIMARY KEY AUTO_INCREMENT,
+    school_term_name VARCHAR(15) NOT NULL
+);
+
+SELECT * FROM student;
 show tables;
